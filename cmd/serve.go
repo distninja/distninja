8ba73c4e6e84e2bc706da4a512d2b03dd1c30ba9
Jia Jia <angersax@sina.com>
2025-07-05 13:14:27 +0800

    fix(cmd): fix error on store database not initialized

--- cmd/serve.go
+++ cmd/serve.go
@@ -23,12 +23,6 @@ var serveCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 		_path := utils.ExpandTilde(storePath)
-		if _, err := os.Stat(_path); err == nil {
-			if entries, err := os.ReadDir(_path); err == nil && len(entries) > 0 {
-				_, _ = fmt.Fprintln(os.Stderr, "store path contains files:", storePath)
-				os.Exit(1)
-			}
-		}
 		if err := runServe(ctx, _path); err != nil {
 			_, _ = fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
